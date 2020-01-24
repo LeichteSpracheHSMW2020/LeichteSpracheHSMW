c54a95674f43fb01667fba5210df969008409c67
Dominik B <vXDarkestNightXv@gmail.com>
2020-01-24 02:20:46 +0100

    Discord Bot Created

---
+++ Discord/Checklist/Checklist/Modules/Commands.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checklist.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping() {
+            await ReplyAsync("Pong");
+        }
+
+        [Command("checklist")]
+        public async Task CreateChecklist([Remainder] string value)
+        {
+            string[] values = value.Split(' ');
+            string text = "";
+            foreach (string v in values) {
+                text += "||:o:||          " + v + "\n";
+            }
+            await ReplyAsync(text);
+        }
+    }
+}
